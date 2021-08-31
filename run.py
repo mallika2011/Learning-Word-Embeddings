5a954062df91d07f53f95102d126055f57955cbc
Mallika <mallika2011@yahoo.com>
2021-09-01 02:09:57 +0530

    Save pretrained vecs

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ from frequency_based_training import *
 # Defining key global variables
 CORPUS_FILENAME = sys.argv[1]
 CORPUS = []
+TRAIN_TYPE = sys.argv[2] #0: frequency based, 1: prediction based
 
 
 
@@ -34,10 +35,16 @@ if __name__=='__main__':
     print("Total number of reviews = ", len(CORPUS))
 
     #train word vectors using the frequency based co-occurence matrix
-    freq_train = FreqTrain(CORPUS)
-    freq_train.create_vocabulary()
-    _ = freq_train.generate_comatrix(3)
-    freq_vectors = freq_train.perform_svd(10)
+
+    if TRAIN_TYPE == 0:
+        freq_train = FreqTrain(CORPUS)
+        freq_train.create_vocabulary()
+        _ = freq_train.generate_comatrix(3)
+        freq_vectors = freq_train.perform_svd(10)
+        np.save('freq_word_vectors', np.array(freq_vectors))
+
+    elif TRAIN_TYPE == 1:
+        print("Not implemented yet ...")
 
 
 
