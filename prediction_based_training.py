6fdf14aedc6a468f60a5550d2b3ddc5eb7654205
Mallika <mallika2011@yahoo.com>
2021-09-06 22:11:41 +0530

    Add batched training and fix quick testing

--- prediction_based_training.py
+++ prediction_based_training.py
@@ -15,6 +15,7 @@ from sklearn.decomposition import PCA
 from scipy.sparse import csr_matrix
 import torch
 import torch.nn as nn
+import torch.utils.data as data_utils
 
         
 
@@ -30,19 +31,19 @@ class CBOW(torch.nn.Module):
         
     def forward(self, context_words):
 
-        # print("context words size ", context_words.size())
-        embedded_context_words = self.embedding(context_words)  # (context_size x embedding_size), 
-        # print("embedded words size", embedded_context_words.size())
-        output1 = self.W1(embedded_context_words)               # (context_size X hidden_layer)
-        # print("outputs1 size", output1.size())
+        #print("context words size ", context_words.size())
+        embedded_context_words = self.embedding(context_words)  # (batch_size, context_size x embedding_size), 
+        #print("embedded words size", embedded_context_words.size())
+        output1 = self.W1(embedded_context_words)               # (batch_size, context_size X hidden_layer)
+        #print("outputs1 size", output1.size())
         
-        mean_output = torch.mean(output1, dim=0).view(1,-1)                # (1 x hidden_layer_size)
-        # print("mean output size ", mean_output.size())
+        mean_output = torch.mean(output1, dim=1)                # (batch_size x hidden_layer_size)
+        #print("mean output size ", mean_output.size())
 
-        output2 = self.W2(mean_output)                          # (1 x vocab_size)
-        # print("outputs2 size", output2.size())
+        output2 = self.W2(mean_output)                          # (batch_size x vocab_size)
+        #print("outputs2 size", output2.size())
         score_vector = self.log_softmax(output2)
-        # print("score_vector_size", score_vector.size())
+        #print("score_vector_size", score_vector.size())
 
         return score_vector
 
@@ -51,19 +52,10 @@ class PredTrain():
     def __init__(self, tokenized_corpus, word2ind, ind2word, vocab_size) -> None:
         super().__init__()
         self.tokenized_corpus = tokenized_corpus
-        # print("lennnnn", len(self.tokenized_corpus))
         self.word2ind = word2ind
         self.ind2word = ind2word
         self.vocab_size = vocab_size
-        # print("self vocabbbbbb", self.vocab_size)
-
-    # def generate_one_hot_vectors(self, words):
-
-    #     one_hot_vectors = np.zeros((len(words), self.vocab_size))
-    #     for i, word in enumerate(words):
-    #         one_hot_vectors[i][self.word2ind[word]] = 1
-
-    #     return one_hot_vectors
+        self.batch_size = 1024
 
     
     def generate_context_center_data(self, context_size):
@@ -73,6 +65,9 @@ class PredTrain():
         for sentence in tqdm.tqdm(self.tokenized_corpus):
             for i, center_word in enumerate(sentence):
                 
+                if i < context_size or i >= len(sentence) - context_size:  #to enusre that the batches all have same context
+                    continue                                               #possible #TODO: use <unk> tokens if required
+
                 if center_word not in self.word2ind: #skip words that occur less than 5 times
                     continue
                 
@@ -88,12 +83,12 @@ class PredTrain():
 
                     context.append(self.word2ind[sentence[j]])
 
-                # one_hot_context_vectors = self.generate_one_hot_vectors(context)
-                # one_hot_center_vector = self.generate_one_hot_vectors([center_word])
                 self.context_center_data.append((
                     torch.tensor(context, dtype=torch.int64),
                     torch.tensor([self.word2ind[center_word]], dtype=torch.int64)
                     ))          
+
+        self.trainloader = torch.utils.data.DataLoader(self.context_center_data, shuffle=True, batch_size=self.batch_size)
 
     
     def train_cbow(self, embedding_size, hidden_layer_size, model_file_path, embedding_file_path, window_size, num_epochs):
@@ -118,10 +113,10 @@ class PredTrain():
             
             train_loss = 0
 
-            for context_vectors, center_vector in tqdm.tqdm(self.context_center_data):
+            for data in self.trainloader:
 
-                context_vectors = context_vectors.to(device)
-                center_vector = center_vector.to(device)
+                context_vectors = data[0].to(device)
+                center_vector = data[1].squeeze_().to(device)
 
                 outputs = model(context_vectors)
                 loss = loss_function(outputs, center_vector)
@@ -139,12 +134,12 @@ class PredTrain():
 
         print("Quick Testing")
         context = ['The','new','is', 'great']
-        context_vector = [self.word2ind[word.lower()] for word in context]
+        context_vector = torch.tensor([self.word2ind[word.lower()] for word in context], dtype=torch.int64).view(1, -1)
         a = model(context_vector)
 
         #Print result
         print(f'Context: {context}\n')
-        print(f'Prediction: {self.ind2word[torch.argmax(a[0]).item()]}')
+        print(f'Prediction: {self.ind2word[str(torch.argmax(a[0]).item())]}')
 
 
 
