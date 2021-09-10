9534997e3462c098c67536a4ba991bd2fd2e1fba
Mallika Subramanian <mallika.subramanian@students.iiit.ac.in>
2021-09-10 22:59:20 +0530

    Updated runs and model training with bug fixes & heuristics

--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#SBATCH -A research
-#SBATCH --mem-per-cpu=16G
-#SBATCH --mail-type=END
+#SBATCH -A nlp
+#SBATCH -c 1
+#SBATCH --gres=gpu:1
+#SBATCH --mem-per-cpu=64G
 #SBATCH -o wv_op.txt 
 #SBATCH --job-name=wv_train
 #SBATCH --time=3-00:00:00
 
 
-python3 run.py Electronics_5.json 1
+python3 run.py Electronics_5.json 2
