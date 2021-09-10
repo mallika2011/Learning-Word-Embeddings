ab65e26694cd0d87a55667feb7bddec341e85255
Mallika <mallika2011@yahoo.com>
2021-09-10 23:56:50 +0530

    Add report draft, images, plots

---
+++ REPORT.md
@@ -0,0 +1,64 @@
+# Learning Vector Embeddings for Words
+
+This report describes the methods adopted to train the word embeddings on the *Stanford Amazon Electronics Product Reviews* corpus.
+
+## Analysis for SVD of Co-occurrence based Embeddings:
+
+The dimensionality of each word vector is chosen as **50** and a window size of **5 words** (left and right of the center word) is selected.
+
+### Closest words for the word "Camera":
+| | |
+:-------------------------:|:-------------------------:
+![](./images/camera1.png)  |  ![](./images/camera2.png)
+
+***Observations:***
+
+*  The **custom** model has words such as *photos*, *shoots*, *dslr*, etc. which are semantically very close to the query word "camera".
+
+*  Some exceptions are *opportunities* with the lowest score, indicating that this word occurs several times in similar contexts as the word "camera", and the word *camer* which is a typographical mistake that the model has learnt as well.
+
+### Similar words to a mix of words:
+
+The top most similar (geometrically closest) words for 5 words - adjectives, nouns, verbs combined - are shown below:
+
+***Word : Bought, POS: Verb***
+
+| SVD | CBOW | GENSIM |
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./images/svd/bought.png)  |  ![](./images/cbow0.001/bought.png)  |  ![](./images/gensim/bought.png)
+![](./plots/svd_BOUGHT_custom_model.png)  |  ![](./plots/cbow0.001_BOUGHT_custom_model.png)  |  ![](./plots/svd_BOUGHT_gensim_model.png)
+
+***Word : Broken, POS: Adjective***
+
+| SVD | CBOW | GENSIM |
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./images/svd/broken.png)  |  ![](./images/cbow0.001/broken.png)  |  ![](./images/gensim/broken.png)
+![](./plots/svd_BROKEN_custom_model.png)  |  ![](./plots/cbow0.001_BROKEN_custom_model.png)  |  ![](./plots/svd_BROKEN_gensim_model.png)
+
+***Word : Computer, POS: Noun***
+
+| SVD | CBOW | GENSIM |
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./images/svd/computer.png)  |  ![](./images/cbow0.001/computer.png)  |  ![](./images/gensim/computer.png)
+![](./plots/svd_COMPUTER_custom_model.png)  |  ![](./plots/cbow0.001_COMPUTER_custom_model.png)  |  ![](./plots/svd_COMPUTER_gensim_model.png)
+
+***Word : Excellent, POS: Adjective***
+
+| SVD | CBOW | GENSIM |
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./images/svd/excellent.png)  |  ![](./images/cbow0.001/excellent.png)  |  ![](./images/gensim/excellent.png)
+![](./plots/svd_EXCELLENT_custom_model.png)  |  ![](./plots/cbow0.001_EXCELLENT_custom_model.png)  |  ![](./plots/svd_EXCELLENT_gensim_model.png)
+
+***Word : iPhone, POS: Noun***
+
+| SVD | CBOW | GENSIM |
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./images/svd/iphone.png)  |  ![](./images/cbow0.001/iphone.png)  |  ![](./images/gensim/iphone.png)
+![](./plots/svd_IPHONE_custom_model.png)  |  ![](./plots/cbow0.001_IPHONE_custom_model.png)  |  ![](./plots/svd_IPHONE_gensim_model.png)
+
+***Word : Terrible, POS: Adjective***
+
+| SVD | CBOW | GENSIM |
+:-------------------------:|:-------------------------:|:-------------------------:
+![](./images/svd/terrible.png)  |  ![](./images/cbow0.001/terrible.png)  |  ![](./images/gensim/terrible.png)
+![](./plots/svd_TERRIBLE_custom_model.png)  |  ![](./plots/cbow0.001_TERRIBLE_custom_model.png)  |  ![](./plots/svd_TERRIBLE_gensim_model.png)
--- analysis.py
+++ analysis.py
@@ -12,6 +12,7 @@ import json
 import sys
 import tqdm
 from scipy import spatial
+import pandas as pd
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from gensim.models import Word2Vec
@@ -20,10 +21,18 @@ from gensim.test.utils import datapath, get_tmpfile
 from gensim.models import KeyedVectors
 import gensim.downloader
 from gensim.scripts.glove2word2vec import glove2word2vec
+from sklearn.decomposition import PCA
+from colorama import Fore, Back, Style
 
+METHOD = sys.argv[1]
+EMBEDDING_PATH = sys.argv[2]
 
 def get_top_10_words_custom_model(query_word):
-    vectors = np.load("./embeddings/freq_word_vectors_with_sampling.npy")
+
+    query_word = query_word.lower()
+
+    vectors = np.load(EMBEDDING_PATH)
+    #vectors = vectors.T
 
     with open("./vocab_files/word2ind.json", 'r') as f:
         word2ind = json.load(f)
@@ -45,7 +54,7 @@ def get_top_10_words_custom_model(query_word):
     return top_similar_words, np.array(top_similar_vectors)
 
 def get_top_10_words_pretrained_model(query_word):
-
+    query_word = query_word.lower()
     top_similar_words = glove_vectors.most_similar(query_word, topn=11)
     top_similar_vectors = [glove_vectors[word] for word, _ in top_similar_words]
     
@@ -65,23 +74,42 @@ def plot_closest_words(input_vecs_list, input_words_list, query_word):
 
     for i, (input_vecs, input_words) in enumerate(zip(input_vecs_list, input_words_list)):
         reduced_vecs = TSNE(
-            perplexity=15, 
-            n_components=2, 
-            init='pca',
-            n_iter=3500, 
-            random_state=32
+                perplexity=25, 
+                n_components=2, 
+                init='pca',
+                n_iter=2500, 
+                random_state=123
             ).fit_transform(input_vecs)
 
+        # pca = PCA(n_components=2)
+        # reduced_vecs = pca.fit_transform(input_vecs)
         
         for point, word in zip(reduced_vecs, input_words):
             x = point[0]
             y = point[1]
             plt.scatter(x, y, marker='o', color=color_list[i])
-            plt.text(x+0.005, y+0.005, word[0], fontsize=12)
+            plt.text(x+0.005, y+0.005, word[0], fontsize=16)
+            plt.title("Top 10 closest/similar words for "+query_word, fontsize=16)
 
         
-    plt.savefig("plots/top_words_for_"+str(query_word)+".png")
+    plt.savefig("plots/"+METHOD+"_"+str(query_word)+".png")
 
+
+def print_results(top_closest_words_custom, top_closest_vectors_gensim, word):
+    
+    print("\n----------------------------------")
+    print("For query word:", Style.BRIGHT+Fore.MAGENTA+word+Fore.RESET+Style.RESET_ALL)
+    print("----------------------------------\n")
+
+    print("Closest words via"+Style.BRIGHT+Fore.BLUE+" Custom "+Fore.RESET+Style.RESET_ALL+"model:")
+    df = pd.DataFrame(top_closest_words_custom, columns=['Word', 'Distance'])       
+    print(df) 
+
+    print() 
+
+    print("Closest words via"+Style.BRIGHT+Fore.BLUE+" Gensim "+Fore.RESET+Style.RESET_ALL+"model:")
+    df = pd.DataFrame(top_closest_vectors_gensim, columns=['Word', 'Distance'])       
+    print(df)
 
 if __name__ == '__main__':
 
@@ -89,12 +117,12 @@ if __name__ == '__main__':
 
     print("Performing report analysis...")
     query_words = [
-        "broken",
-        "computer",
-        "bought",
-        "excellent",
-        "iphone",
-        "terrible"
+        "BROKEN",
+        "COMPUTER",
+        "BOUGHT",
+        "EXCELLENT",
+        "IPHONE",
+        "TERRIBLE"
     ]
 
     #obtain and plot the top closest words for these 5 words via gensim and custom model
@@ -106,25 +134,19 @@ if __name__ == '__main__':
 
     for word in query_words:
 
-        print("\n--------------------------")
-        print("For query word:", word)
+
 
         top_closest_words_custom, top_closest_vectors_custom = get_top_10_words_custom_model(word)
         plot_closest_words([top_closest_vectors_custom], [top_closest_words_custom], word+"_custom_model")
         query_words_top_vectors_custom.append(top_closest_vectors_custom)
         query_words_top_words_custom.append(top_closest_words_custom)
-        print("Closest words via custom model:")
-        for el in top_closest_words_custom:
-            print(el)        
 
-        print()
         top_closest_words_gensim, top_closest_vectors_gensim = get_top_10_words_pretrained_model(word)
         plot_closest_words([top_closest_vectors_gensim], [top_closest_words_gensim], word+"_gensim_model")
         query_words_top_vectors_gensim.append(top_closest_vectors_gensim)
         query_words_top_words_gensim.append(top_closest_words_gensim)
-        print("Closest words via gensim model:")
-        for el in top_closest_words_gensim:
-            print(el)  
+
+        print_results(top_closest_words_custom, top_closest_words_gensim, word)
 
     plot_closest_words(query_words_top_vectors_custom, query_words_top_words_custom, "combined_custom_model")
     plot_closest_words(query_words_top_vectors_gensim, query_words_top_words_gensim, "combined_gensim_model")
@@ -132,9 +154,11 @@ if __name__ == '__main__':
 
     #obtain and plot the top closest words for "CAMERA" via gensim and custom model 
     print("\n\n********************** Question 2 *************************")
-    report_q2_word = "camera"
-    top_closest_words_custom_camera, top_closest_vectors_custom_camera = get_top_10_words_custom_model(word)
-    top_closest_words_gensim, top_closest_vectors_gensim = get_top_10_words_pretrained_model(word)
+    report_q2_word = "CAMERA"
+    top_closest_words_custom_camera, top_closest_vectors_custom_camera = get_top_10_words_custom_model(report_q2_word)
+    top_closest_words_gensim, top_closest_vectors_gensim = get_top_10_words_pretrained_model(report_q2_word)
+    print_results(top_closest_words_custom_camera, top_closest_words_gensim, report_q2_word)
+
     plot_closest_words([top_closest_vectors_custom_camera], [top_closest_words_custom_camera], "camera_custom_model")
     plot_closest_words([top_closest_vectors_custom_camera], [top_closest_words_custom_camera], "camera_gensim_model")
 
